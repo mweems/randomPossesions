72d02fda23fac7259cd9a39222d6c99f06a593d5
Matt Weems <mweems@thoughtworks.com>
2013-12-09 10:26:12 -0500

    doing challenges at end of chapter

---
+++ RandomPossessions/BNRContainer.h
@@ -0,0 +1,15 @@
+//
+//  BNRContainer.h
+//  RandomPossessions
+//
+//  Created by mweems on 12/8/13.
+//  Copyright (c) 2013 mweems. All rights reserved.
+//
+
+import <Foundation/Foundation.h>
+
+@interface BNRContainer : BNRItem
+
+
+
+@end
